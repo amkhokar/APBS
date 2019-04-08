6c2435b0ac6704daebfa3212b185d577b65a5019
Ahmed Khokar <amkhokar@gmail.com>
2019-04-08 01:08:32 -0700

    added CORS policy for dev server

---
+++ PBScraper/Controllers/PBScrapeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PBScraper.Models;
+
+namespace PBScraper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PBScrapeController : ControllerBase
+    {
+        // GET: api/PBScrape
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List <PBScrape> allScrapes = PBScrape.GetAll();
+            List <string> allKeys = new List <string>();
+            for (int i = 0; i < allScrapes.Count; i++)
+            {
+                allKeys.Add(allScrapes[i].GetKeyword());
+            }
+            return allKeys;
+        }
+
+        // GET: api/PBScrape/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PBScrape
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PBScrape/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PBScraperTests/PBScrapeModelsTest.cs
@@ -0,0 +1,124 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using PBScraper.Models;
+
+namespace PBScraperTests
+{
+    [TestClass]
+    public class PBScraperModelTests
+    {
+        [TestMethod]
+        public void PBScrape_SavesStaticVariables_IsTrue()
+        {
+            PBScrape newScrape = new PBScrape();
+            
+            string Keyword = newScrape.GetKeyword();
+            bool TestBool = false;
+            if (Keyword == "Bongos")
+            {
+                TestBool = true;
+            }
+            Assert.AreEqual(TestBool, true);
+        }
+        [TestMethod]
+        public void PBScrape_CountsDatabaseEntries_1()
+        {
+            PBScrape.ClearAll();
+            PBScrape newScrape = new PBScrape();
+            newScrape.Save();
+            List<PBScrape> allScrapes = PBScrape.GetAll();
+            int count = allScrapes.Count;
+            Assert.AreEqual(1, count);
+            
+        }
+
+        [TestMethod]
+        public void PBScrape_SetsAndRetrievesEmail_IsTrue()
+        {
+            //Arrange
+            PBScrape newScrape = new PBScrape("Email test");
+            //Act
+            newScrape.SetEmail("hanley.doggo@outlook.com");
+            //Assert
+            Assert.AreEqual("hanley.doggo@outlook.com", newScrape.GetEmail());
+        }
+        [TestMethod]
+        public void PBScrape_SetsAndRetrievesPhone_IsTrue()
+        {
+            //Arrange
+            PBScrape newScrape = new PBScrape("Phone test");
+            //Act
+            newScrape.SetPhone("1-800-453-9999");
+            //Assert
+            Assert.AreEqual("1-800-453-9999", newScrape.GetPhone());
+        }
+        [TestMethod]
+        public void PBScrape_SetsAndRetrievesKeyword_IsTrue()
+        {
+            //Arrange
+            PBScrape newScrape = new PBScrape();
+            //Assert
+            newScrape.SetKeyword("Seattle Flowers");
+            Assert.AreEqual("Seattle Flowers", newScrape.GetKeyword());
+        }
+        [TestMethod]
+        public void PBScrape_GetsURLTitle_IsTrue()
+        {
+            //Arrange
+            string Keyword = "Wikipedia test";
+            string Url = "https://www.wikipedia.org";
+            PBScrape newScrape = new PBScrape(Keyword);
+            newScrape.SetUrl(Url);
+            //Act
+            object ParseObject = newScrape.GetTitleHtml(newScrape.GetUrl());
+            //Assert
+            Assert.AreEqual("Wikipedia", ParseObject);
+        }
+        [TestMethod]
+        public void PBScrape_RetrievedURLSInList_NotEqualToZero()
+        {
+            //Arrange
+            string Keyword = "Sandwhich";
+            PBScrape newScrape = new PBScrape(Keyword);
+            //Act
+            newScrape.GetGoogleResults(newScrape.GetKeyword());
+            List<string> urlList = newScrape.GetUrls();
+            //Assert
+            Assert.AreNotEqual(0, urlList);
+        }
+        [TestMethod]
+        public void PBScrape_FindEmail_IsTrue()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        [TestMethod]
+        public void PBScrape_FindPhoneNumber_IsTrue()
+        {
+
+        }
+        [TestMethod]
+        public void PBScrape_SaveUrlList_IsTrue()
+        {
+            ////Arrange
+            //string Keyword = "Seattle Bouldering";
+            //PBScrape newScrape = new PBScrape(Keyword);
+            ////Act
+            //newScrape.GetGoogleResults(newScrape.GetKeyword());
+            //List<string> urlList = newScrape.GetUrls();
+            //newScrape.SaveURLInstanceList(urlList);
+            //PBScrape.ClearAll();
+            ////Assert
+            //Assert.Fail();
+        }
+        [TestMethod]
+        public void PBScrape_RetrievesBingAPI_IsTrue()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
